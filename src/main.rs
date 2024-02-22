d7e0220212ac8d7749517b31848836501649d5ba
Yannis Storrer <yannis.storrer@web.de>
2024-02-22 22:41:50 +0100

    Adjusted output

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::env;
+use std::{env, path::PathBuf};
+
+use bandcamp::TrackMetadata;
+use dirs::audio_dir;
 mod bandcamp;
 mod downloader;
 
@@ -14,10 +17,19 @@ fn main() {
         }
         let download_links = download_links.unwrap();
         println!("Starting download. Found {} tracks...", download_links.len());
-        for (link, metadata) in download_links {
+        for (link, metadata) in download_links.to_owned() {
             let downloaded_file = downloader::download_from_link(link).unwrap();
             downloader::move_and_tag_file(downloaded_file, metadata.to_owned()).unwrap();
             println!("Downloaded {} {} by {}", metadata.track_number, metadata.name, metadata.artist);
         }
+        println!("Finished downloading to {:?}", get_download_dir(&download_links.get(0).unwrap().1).into_os_string());
     }
+}
+
+fn get_download_dir(metadata: &TrackMetadata) -> PathBuf {
+    let mut download_dir = audio_dir().unwrap();
+    download_dir.push("bandrip");
+    download_dir.push(metadata.artist.to_owned());
+    download_dir.push(metadata.album.to_owned());
+    download_dir
 }
