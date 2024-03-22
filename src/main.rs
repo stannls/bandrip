9516e319bf689d593ad23478608bf22a0f951e5f
Yannis Storrer <yannis.storrer@web.de>
2024-03-22 19:56:13 +0100

    Bumped version

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod downloader;
 fn main() {
     let matches = command!()
         .name("bandip")
-        .version("0.1.2")
+        .version("0.2.0")
         .about("A simple bandcamp downloader.")
         .arg(
             arg!(<LINK>)
