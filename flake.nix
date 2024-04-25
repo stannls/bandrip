2756da475f69d6277b43c2b754defeb2893e0a3a
Yannis Storrer <yannis.storrer@web.de>
2024-04-25 12:59:09 +0200

    Added devshell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+	rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          rustToolchain = pkgs.rust-bin.nightly.latest.default;
+          # new! 👇
+          nativeBuildInputs = with pkgs; [ rustToolchain pkg-config ];
+          # also new! 👇
+          buildInputs = with pkgs; [ openssl alsa-lib pkg-config ];
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            # 👇 and now we can just inherit them
+            inherit buildInputs nativeBuildInputs;
+          };
+        }
+      );
+}
