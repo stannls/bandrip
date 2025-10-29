abe9012a717ec24e5286d749aef6d4d0945c0002
Yannis Storrer <yannis.storrer@web.de>
2025-10-29 18:28:46 +0100

    Update Dependencies and use unstable toolchain

--- flake.nix
+++ flake.nix
@@ -3,12 +3,30 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+    nixpkgs-mozilla = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
   };
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, naersk, nixpkgs-mozilla }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = (import nixpkgs) {
+          inherit system;
+
+          overlays = [
+            (import nixpkgs-mozilla)
+          ];
+        };
         naersk-lib = pkgs.callPackage naersk { };
+        toolchain = (pkgs.rustChannelOf {
+          rustToolchain = ./rust-toolchain.toml;
+          sha256 = "sha256-YZARSWuUvkFUPWNVzEa0g6ii8ceU0TqmYMd4YzLxH/U=";
+        }).rust;
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
       in
       {
         defaultPackage = naersk-lib.buildPackage {
@@ -16,7 +34,7 @@
           buildInputs = with pkgs; [ openssl pkg-config ];
 		};
         devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy pkg-config openssl ];
+          buildInputs = [ toolchain rustfmt pre-commit rustPackages.clippy pkg-config openssl ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
       }
